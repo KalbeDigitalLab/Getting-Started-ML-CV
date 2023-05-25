2f68f492e98b927e7d6c473f2508c4d65a8db13f
haritsah <m.haritsah@mail.ugm.ac.id>
2023-05-25 22:27:28 +0700

    Add experiment tracking example

---
+++ 02_Model_Development/src/dataset.py
@@ -0,0 +1,187 @@
+import glob
+import os
+from typing import List, Optional, Tuple
+
+import cv2
+import numpy as np
+import torch
+from lightning import LightningDataModule
+from torch.utils.data import DataLoader, Dataset
+from torchvision.transforms import transforms
+
+
+RGB_MEAN = [0.51442681, 0.43435301, 0.33421855]
+RGB_STD = [0.24099932, 0.246478, 0.23652802]
+INPUT_SIZE = (384, 384)
+
+
+TRAIN_TRANSFORMATION = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomRotation(45),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomVerticalFlip(),
+    transforms.CenterCrop(INPUT_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=RGB_MEAN, std=RGB_STD)
+])
+
+VAL_TRANSFORMATION = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.CenterCrop(INPUT_SIZE),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=RGB_MEAN, std=RGB_STD)
+])
+
+
+class ImageFolderDataset(Dataset):
+    """Strutured Image Folder Dataset
+
+    Parameters
+    ----------
+    root_dir : str
+        Path to image root directory
+    transform : Optional[torchvision.transforms.Compose], optional
+        Data augmentation pipeline, by default None
+    """
+
+    def __init__(self, root_dir: str, stage: Optional[str] = None, transform: Optional[transforms.Compose] = None):
+        if stage is not None:
+            self.root_dir = os.path.join(root_dir, stage)
+        else:
+            self.root_dir = root_dir
+        if not os.path.exists(root_dir):
+            raise RuntimeError(f'Path to dataset is not valid')
+        self.labels_name = os.listdir(self.root_dir)
+        self.labels_name.sort()
+        self.list_images =  glob.glob(f'{self.root_dir}/**/*.jpg')
+        self.transform = transform
+
+    def __len__(self) -> int:
+        """Get number of images."""
+        return len(self.list_images)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        """Get sample for the current idx."""
+        img_path = self.list_images[idx]
+
+        # NOTE: cv2 read image as BGR.
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # Convert class name to label 0-10
+        # NOTE: root_path/class_name/image_file.jpg
+        class_name = img_path.split('/')[-2]
+        label_index = self.labels_name.index(class_name)
+        label_index = [label_index]
+
+        # Apply transformations
+        if self.transform:
+            image = self.transform(image)
+
+        # If no transformations, convert to C,H,W
+        # Normalize to [0,1]
+        if isinstance(image, np.ndarray):
+            image = np.transpose(image, (1, 2, 0))
+            image = torch.from_numpy()
+            image /= 255.0
+
+        return image, torch.tensor(label_index, dtype=torch.int64)
+
+
+class Food101LitDatamodule(LightningDataModule):
+    """LightningDataModule for Food101 Data Pipeline.
+
+    Read the docs:
+        https://lightning.ai/docs/pytorch/latest/data/datamodule.html
+
+    Parameters
+    ----------
+    data_dir : str, optional
+        FiftyOne dataset directory, by default 'data/'
+    input_size : List[int], optional
+        Input model size, by default [600, 500]
+    batch_size : int, optional
+        Number of training batch size, by default 64
+    num_workers : int, optional
+        Number of worksers to process data, by default 0
+    pin_memory : bool, optional
+        Enable memory pinning, by default False
+    """
+
+    def __init__(
+        self,
+        data_dir: str = 'data/',
+        input_size: Tuple[int, int] = (600, 500),
+        batch_size: int = 64,
+        num_workers: int = 0,
+        pin_memory: bool = False,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.data_train: Optional[Dataset] = None
+        self.data_val: Optional[Dataset] = None
+        self.data_test: Optional[Dataset] = None
+
+    @property
+    def num_classes(self):
+        """Get number of classes."""
+        return 10
+
+    def setup(self, stage: Optional[str] = None):
+        """Load the data with specified stage."""
+        if stage in ['train', 'fit', None] and self.data_train is None:
+            self.data_train = ImageFolderDataset(
+                root_dir=self.hparams.data_dir, stage='train', transform=TRAIN_TRANSFORMATION)
+            if len(self.data_train) == 0:
+                raise ValueError('Train dataset is empty.')
+        if stage in ['validation', 'test', 'fit', None]:
+            if self.data_val is None:
+                self.data_val = ImageFolderDataset(
+                    root_dir=self.hparams.data_dir, stage='valid', transform=VAL_TRANSFORMATION)
+                if len(self.data_val) == 0:
+                    raise ValueError('Validation dataset is empty.')
+            if self.data_test is None:
+                self.data_test = ImageFolderDataset(
+                    root_dir=self.hparams.data_dir, stage='valid', transform=VAL_TRANSFORMATION)
+                if len(self.data_test) == 0:
+                    raise ValueError('Test dataset is empty.')
+        if stage == 'predict':
+            if self.data_test is None:
+                self.data_predict = ImageFolderDataset(
+                    root_dir=self.hparams.data_dir, transform=VAL_TRANSFORMATION)
+                if len(self.data_predict) == 0:
+                    raise ValueError('Predict dataset is empty.')
+
+    def train_dataloader(self):
+        """Get train dataloader."""
+        return DataLoader(
+            dataset=self.data_train,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=True,
+        )
+
+    def val_dataloader(self):
+        """Get validation dataloader."""
+        return DataLoader(
+            dataset=self.data_val,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
+
+    def test_dataloader(self):
+        """Get test dataloader."""
+        return DataLoader(
+            dataset=self.data_test,
+            batch_size=self.hparams.batch_size,
+            num_workers=self.hparams.num_workers,
+            pin_memory=self.hparams.pin_memory,
+            shuffle=False,
+        )
---
+++ 02_Model_Development/src/models.py
@@ -0,0 +1,251 @@
+from typing import Any, Optional
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchmetrics as tm
+from lightning import LightningModule
+
+
+class BasicBlock(nn.Module):
+    """ResNet Basic Block.
+
+    Parameters
+    ----------
+    in_channels : int
+        Number of input channels
+    out_channels : int
+        Number of output channels
+    stride : int, optional
+        Convolution stride size, by default 1
+    identity_downsample : Optional[torch.nn.Module], optional
+        Downsampling layer, by default None
+    """
+
+    def __init__(self,
+                in_channels: int,
+                out_channels: int,
+                stride: int = 1,
+                identity_downsample: Optional[torch.nn.Module] = None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels,
+                              out_channels,
+                              kernel_size = 3,
+                              stride = stride,
+                              padding = 1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(out_channels,
+                              out_channels,
+                              kernel_size = 3,
+                              stride = 1,
+                              padding = 1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        self.identity_downsample = identity_downsample
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Apply forward computation."""
+        identity = x
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+
+        # Apply an operation to the identity output.
+        # Useful to reduce the layer size and match from conv2 output
+        if self.identity_downsample is not None:
+            identity = self.identity_downsample(identity)
+        x += identity
+        x = self.relu(x)
+        return x
+
+
+class ResNet18(nn.Module):
+    """Construct ResNet-18 Model.
+
+    Parameters
+    ----------
+    input_channels : int
+        Number of input channels
+    num_classes : int
+        Number of class outputs
+    """
+
+    def __init__(self, input_channels, num_classes):
+
+        super(ResNet18, self).__init__()
+        self.conv1 = nn.Conv2d(input_channels,
+                               64, kernel_size = 7,
+                              stride = 2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size = 3,
+                                   stride = 2,
+                                   padding = 1)
+
+        self.layer1 = self._make_layer(64, 64, stride = 1)
+        self.layer2 = self._make_layer(64, 128, stride = 2)
+        self.layer3 = self._make_layer(128, 256, stride = 2)
+        self.layer4 = self._make_layer(256, 512, stride = 2)
+
+        # Last layers
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def identity_downsample(self, in_channels: int, out_channels: int) -> nn.Module:
+        """Downsampling block to reduce the feature sizes."""
+        return nn.Sequential(
+             nn.Conv2d(in_channels,
+                       out_channels,
+                       kernel_size = 3,
+                       stride = 2,
+                       padding = 1),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def _make_layer(self, in_channels: int, out_channels: int, stride: int) -> nn.Module:
+        """Create sequential basic block."""
+        identity_downsample = None
+
+        # Add downsampling function
+        if stride != 1:
+            identity_downsample = self.identity_downsample(in_channels, out_channels)
+
+        return nn.Sequential(
+                    BasicBlock(in_channels, out_channels, identity_downsample=identity_downsample, stride=stride),
+                    BasicBlock(out_channels, out_channels)
+                    )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.fc(x)
+        return x
+
+
+class ClassificationLightningModule(LightningModule):
+    """Model training pipeline for Food101 classification.
+
+    Parameters
+    ----------
+    net : torch.nn.Module
+        The model module or configuration
+    num_classes : int, optional
+        Number of output classes, by default 10
+    lr : float, optional
+        Optimizer learning rate, by default 0.00001
+    weight_decay : float, optional
+        Optimizer weight decay, by default 0.0005
+    """
+
+    def __init__(
+        self,
+        net: torch.nn.Module,
+        num_classes: int = 10,
+        lr: float = 0.00001,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False)
+
+        self.net = net
+
+        # loss function
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        # metric objects for calculating and averaging accuracy across batches
+        self.train_acc = tm.Accuracy(task='multiclass', num_classes=num_classes)
+
+        self.val_metrics = tm.MetricCollection({
+            'acc': tm.Accuracy(task='multiclass', num_classes=num_classes),
+            'prec': tm.Precision(task='multiclass', num_classes=num_classes),
+            'rec': tm.Recall(task='multiclass', num_classes=num_classes),
+            'auroc': tm.AUROC(task='multiclass', num_classes=num_classes),
+            'f1': tm.F1Score(task='multiclass', num_classes=num_classes),
+        })  # type: ignore
+
+        self.test_metrics = self.val_metrics.clone()
+
+    def reset_metrics(self):
+        self.train_acc.reset()
+        self.val_metrics.reset()
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+
+    def on_train_start(self):
+        # by default lightning executes validation step sanity checks before training starts,
+        # so it's worth to make sure validation metrics don't store results from these checks
+        self.reset_metrics()
+
+    def model_step(self, batch: Any):
+        images, targets = batch
+        targets = targets.squeeze().long() # convert to 1D
+        logits = self.forward(images)
+        loss = self.criterion(logits, targets)
+        preds = F.softmax(logits, dim=1)
+        return loss, preds, targets
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.model_step(batch)
+
+        # update and log metrics
+        acc = self.train_acc(preds, targets)
+        self.log('train/loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('train/acc', acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        # return loss or backpropagation will fail
+        return loss
+
+    def on_train_epoch_start(self):
+        self.reset_metrics()
+
+    def validation_step(self, batch: Any, batch_idx: int) -> None:
+        loss, preds, targets = self.model_step(batch)
+
+        # update and log metrics
+        metrics = self.val_metrics(preds, targets)
+        self.log('val/loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('val/acc', metrics['acc'], on_step=False, on_epoch=True, prog_bar=True)
+        self.log('val/prec', metrics['prec'], on_step=False, on_epoch=True, prog_bar=False)
+        self.log('val/rec', metrics['rec'], on_step=False, on_epoch=True, prog_bar=False)
+        self.log('val/auroc', metrics['auroc'], on_step=False, on_epoch=True, prog_bar=False)
+        self.log('val/f1', metrics['f1'], on_step=False, on_epoch=True, prog_bar=True)
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.model_step(batch)
+
+        # update and log metrics
+        metrics = self.test_metrics(preds, targets)
+        self.log('test/loss', loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test/acc', metrics['acc'], on_step=False, on_epoch=True, prog_bar=True)
+        self.log('test/prec', metrics['prec'], on_step=False, on_epoch=True, prog_bar=False)
+        self.log('test/rec', metrics['rec'], on_step=False, on_epoch=True, prog_bar=False)
+        self.log('test/auroc', metrics['auroc'], on_step=False, on_epoch=True, prog_bar=False)
+        self.log('test/f1', metrics['f1'], on_step=False, on_epoch=True, prog_bar=True)
+
+    def configure_optimizers(self):
+        """Configure the optimizer and scheduler to use.
+
+        Examples:
+            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
+        """
+        optimizer = torch.optim.Adam(
+            params=self.parameters(),
+            lr=self.hparams.lr,
+        )
+
+        return optimizer
